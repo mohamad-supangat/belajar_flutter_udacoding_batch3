89fb3358df546a5f5c71b8c2e56eb84207f82720
Mohamad Supangat <supangatoy@gmail.com>
2020-10-20 10:41:53 +0700

    move Scallfold body to Singlechild scrollview

--- week1/Task1/lib/pages/HomePage.dart
+++ week1/Task1/lib/pages/HomePage.dart
@@ -10,80 +10,82 @@ class HomePage extends StatelessWidget {
         onPressed: _launchURL,
         child: Icon(Icons.account_circle_sharp),
       ),
-      body: Column(
-        children: [
-          Image.asset('assets/images/image843.png'),
-          Container(
-            padding: EdgeInsets.all(0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    width: 200,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Selamat datang di Dapor Merah dimana kamu dapat menikmati masakan mewah ala resto bingtang 5',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => RegisterPage(),
-                          ),
-                        );
-                      },
-                      color: Colors.red,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: BorderSide(color: Colors.red)),
-                      child: Text('Pendaftaran'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/image843.png'),
+            Container(
+              padding: EdgeInsets.all(0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/logo.png',
+                      width: 200,
                     ),
-                    SizedBox(
-                      width: 10,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Selamat datang di Dapor Merah dimana kamu dapat menikmati masakan mewah ala resto bingtang 5',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
                     ),
-                    RaisedButton(
-                      onPressed: () => {},
-                      color: Colors.red,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: BorderSide(color: Colors.red)),
-                      child: Text('Edit Profile'),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  'Aplikasi ini dibuat untuk memenuhi Task 1 Week 1 Udacoding flutterBatch 3',
-                  textAlign: TextAlign.center,
-                  // style: TextStyle(fontSize: 20),
-                )
-              ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => RegisterPage(),
+                            ),
+                          );
+                        },
+                        color: Colors.red,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(color: Colors.red)),
+                        child: Text('Pendaftaran'),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      RaisedButton(
+                        onPressed: () => {},
+                        color: Colors.red,
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            side: BorderSide(color: Colors.red)),
+                        child: Text('Edit Profile'),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Aplikasi ini dibuat untuk memenuhi Task 1 Week 1 Udacoding flutterBatch 3',
+                    textAlign: TextAlign.center,
+                    // style: TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
