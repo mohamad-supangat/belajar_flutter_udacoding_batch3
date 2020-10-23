62289c56548659430c39079f7467d6b9c07e7250
Mohamad Supangat <supangatoy@gmail.com>
2020-10-23 10:46:50 +0700

    #week2 #task3 Card with date picker

--- week2/task3/lib/main.dart
+++ week2/task3/lib/main.dart
@@ -6,112 +6,172 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final _notifier = ValueNotifier<ThemeModel>(ThemeModel(ThemeMode.light));
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return ValueListenableBuilder<ThemeModel>(
+      valueListenable: _notifier,
+      builder: (_, model, __) {
+        final mode = model.mode;
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData.light(), // Provide light theme.
+          darkTheme: ThemeData.dark(), // Provide dark theme.
+          themeMode: mode, // Decides which theme to show.
+          home: Scaffold(
+            body: SingleChildScrollView(
+              child: SafeArea(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  child: CountingForm(),
+                ),
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.brown,
+              child: Icon(Icons.brightness_4),
+              onPressed: () {
+                _notifier.value = ThemeModel(
+                  mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light,
+                );
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class ThemeModel with ChangeNotifier {
+  final ThemeMode _mode;
+  ThemeMode get mode => _mode;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  ThemeModel(this._mode);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CountingForm extends StatefulWidget {
+  @override
+  _CountingFormState createState() => _CountingFormState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _CountingFormState extends State<CountingForm> {
+  DateTime selectedDate;
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate == null ? DateTime.now() : selectedDate,
+        firstDate: DateTime(1980, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Apa itu',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.brown,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                'Counting weton ?',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.brown,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Merupakan aplikasi penghitung umur, dari tahun, bulan maupun berapa hari terhitung dari tanggal kelahiranmu',
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/pakde.jpeg',
+                        width: 100,
+                      ),
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Kasih tau Pakde Jowo kapan tanggal lahir kamu',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.brown,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 25.0,
+                              ),
+                              FlatButton.icon(
+                                padding: EdgeInsets.only(
+                                    left: 30, right: 30, top: 10, bottom: 10),
+                                shape: RoundedRectangleBorder(
+                                    side: BorderSide(
+                                      color: Colors.brown,
+                                      width: 2,
+                                      style: BorderStyle.solid,
+                                    ),
+                                    borderRadius: BorderRadius.circular(50)),
+                                onPressed: () => {
+                                  _selectDate(context),
+                                },
+                                label: Text(
+                                  selectedDate == null
+                                      ? 'Pilih tanggal'
+                                      : '${selectedDate.day}-${selectedDate.month}-${selectedDate.year}',
+                                ),
+                                icon: Icon(Icons.calendar_today_outlined),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
