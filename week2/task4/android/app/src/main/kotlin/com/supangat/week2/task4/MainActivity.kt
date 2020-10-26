6f2ba02dca9197052a1d2e81ace5ce72f781cd00
Mohamad Supangat <supangatoy@gmail.com>
2020-10-27 05:52:02 +0700

    merge task4 dan task5, init new project

--- week2/task4/android/app/src/main/kotlin/com/supangat/week2/task4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.supangat.week2.task4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ week2/task4_dan_task5/android/app/src/main/kotlin/com/supangat/week2/task4_dan_task5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.supangat.week2.task4_dan_task5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
