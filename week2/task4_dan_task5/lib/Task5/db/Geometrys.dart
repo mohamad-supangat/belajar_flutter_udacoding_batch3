fb57cc4a70cc885e2ed5f21202638a78540dd0d5
Mohamad Supangat <supangatoy@gmail.com>
2020-10-30 13:09:36 +0700

    #week2 #task5 Tambah rumus jajar genjang

--- week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
+++ week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
@@ -80,4 +80,54 @@ final List geometrysDb = [
       ),
     ],
   ),
+  Geometry(
+    nama: 'Jajar Genjang',
+    logo: 'assets/images/task5/jajargenjang.png',
+    deskripsi:
+        'Jajar genjang atau jajaran genjang (bahasa Inggris: parallelogram) adalah bangun datar dua dimensi yang dibentuk oleh dua pasang rusuk yang masing-masing sama panjang dan sejajar dengan pasangannya, dan memiliki dua pasang sudut yang masing-masing sama besar dengan sudut di hadapannya.',
+    rumus: [
+      Rumus(
+        nama: 'Keliling',
+        icon: Icons.crop_square,
+        unit: 'cm',
+        deskripsi: 'K = a + b + c + d',
+        formula: 'a + b + c + d',
+        field: [
+          RumusField(
+            nama: 'Sisi a',
+            key: 'a',
+          ),
+          RumusField(
+            nama: 'Sisi b',
+            key: 'b',
+          ),
+          RumusField(
+            nama: 'Sisi c',
+            key: 'c',
+          ),
+          RumusField(
+            nama: 'Sisi d',
+            key: 'd',
+          ),
+        ],
+      ),
+      Rumus(
+        nama: 'Luas',
+        icon: Icons.aspect_ratio_outlined,
+        deskripsi: 'Luas = alas x tinggi atau L = a x t',
+        formula: '%a * %t',
+        unit: 'cm^2',
+        field: [
+          RumusField(
+            nama: 'Alas',
+            key: 'a',
+          ),
+          RumusField(
+            nama: 'Tinggi',
+            key: 't',
+          ),
+        ],
+      ),
+    ],
+  ),
 ];
