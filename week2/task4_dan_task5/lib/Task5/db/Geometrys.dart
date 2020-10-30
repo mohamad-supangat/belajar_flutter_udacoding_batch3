efb6e55655ee34ad2fbb8848f1acc1f92e4b62fb
Mohamad Supangat <supangatoy@gmail.com>
2020-10-30 13:38:00 +0700

    #week2 #task5 tambah rumus/segitiga.dart

--- week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
+++ week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
@@ -1,9 +1,11 @@
 import './rumus/persegi.dart';
 import './rumus/persegiPanjang.dart';
 import './rumus/jajarGenjang.dart';
+import './rumus/segitiga.dart';
 
 final List geometrysDb = [
   persegi,
   persegiPanjang,
   jajarGenjang,
+  segitiga,
 ];
