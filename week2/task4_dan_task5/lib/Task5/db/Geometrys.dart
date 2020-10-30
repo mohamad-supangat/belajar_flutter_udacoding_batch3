75a217b9abc1c6f48506b46da8f77c007a53097e
Mohamad Supangat <supangatoy@gmail.com>
2020-10-30 12:59:47 +0700

    Tambah db persegi panjang

--- week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
+++ week2/task4_dan_task5/lib/Task5/db/Geometrys.dart
@@ -38,4 +38,46 @@ final List geometrysDb = [
       ),
     ],
   ),
+  Geometry(
+    nama: 'Persegi Panjang',
+    logo: 'assets/images/task5/persegiPanjang.png',
+    deskripsi:
+        'Persegi panjang (bahasa Inggris: rectangle) adalah bangun datar dua dimensi yang dibentuk oleh dua pasang sisi yang masing-masing sama panjang dan sejajar dengan pasangannya, dan memiliki empat buah sudut yang kesemuanya adalah sudut siku-siku.',
+    rumus: [
+      Rumus(
+        nama: 'Keliling',
+        icon: Icons.crop_square,
+        unit: 'cm',
+        deskripsi: 'Keliling = 2 x (Panjang + Lebar) atau K = 2x(p+l)',
+        formula: '2 * (%p + %l)',
+        field: [
+          RumusField(
+            nama: 'Panjang',
+            key: 'p',
+          ),
+          RumusField(
+            nama: 'Lebar',
+            key: 'l',
+          ),
+        ],
+      ),
+      Rumus(
+        nama: 'Luas',
+        icon: Icons.aspect_ratio_outlined,
+        deskripsi: 'Luas = Panjang x Lebar atau L = p x l',
+        formula: '%p * %l',
+        unit: 'cm^2',
+        field: [
+          RumusField(
+            nama: 'Panjang',
+            key: 'p',
+          ),
+          RumusField(
+            nama: 'Lebar',
+            key: 'l',
+          )
+        ],
+      ),
+    ],
+  ),
 ];
